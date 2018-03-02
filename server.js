aacc5d411d64afa5ea032d2bd74b8d535827c4c1
camilo mac <camilobaezcamba@gmail.com>
2018-03-01 23:38:01 -0300

    cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
